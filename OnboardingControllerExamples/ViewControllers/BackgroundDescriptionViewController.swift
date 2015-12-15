7d8ec4a5e4faa664af58567c3beb4cb07ee20b3c
rumori <intonarumori@gmail.com>
2015-12-15 21:43:32 +0100

    added content viewcontrollers

---
+++ OnboardingControllerExamples/ViewControllers/BackgroundDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BackgroundDescriptionViewController.swift
+//  OnboardingControllerExamples
+//
+//  Created by Daniel Langh on 15/12/15.
+//  Copyright © 2015 rumori. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundDescriptionViewController: UIViewController {
+
+    @IBOutlet var button:UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // show the onboarding background by making our view transparent
+        self.view.backgroundColor = UIColor.clearColor()
+        
+        // style the button
+        self.button?.backgroundColor = UIColor.blueColor()
+        self.button?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        self.button?.layer.cornerRadius = 30.0
+    }
+    
+    @IBAction func next() {
+        self.onboardingController()?.moveToNext(true)
+    }
+}
---
+++ OnboardingControllerExamples/ViewControllers/LocationSharingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LocationSharingViewController.swift
+//  OnboardingControllerExamples
+//
+//  Created by Daniel Langh on 15/12/15.
+//  Copyright © 2015 rumori. All rights reserved.
+//
+
+import UIKit
+
+class LocationSharingViewController: UIViewController {
+
+    @IBOutlet var bodyLabel:UILabel?
+    @IBOutlet var skipButton:UIButton?
+    @IBOutlet var locationButton:UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // show the onboarding background by making our view transparent
+        self.view.backgroundColor = UIColor.clearColor()
+        
+        // style the buttons
+        self.skipButton?.backgroundColor = UIColor.blueColor()
+        self.skipButton?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        self.skipButton?.layer.cornerRadius = 30.0
+
+
+        self.locationButton?.backgroundColor = UIColor.blueColor()
+        self.locationButton?.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        self.locationButton?.layer.cornerRadius = 30.0
+    }
+    
+    @IBAction func requestLocationAccess() {
+        // put your custom code here to request location access
+        // for the sake of simplicity we use an alert here for this example
+        
+        let alertController = UIAlertController(title: "Location alert example", message: "Implement your location request logic in your custom viewcontroller", preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func next() {
+        self.onboardingController()?.moveToNext(true)
+    }
+}
