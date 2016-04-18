86cd4fc8cbf94d378fa1e889880fe5846fda98c7
rumori <intonarumori@gmail.com>
2016-04-18 12:32:29 +0200

    added failing test

--- OnboardingControllerExamplesTests/OnboardingControllerExamplesTests.swift
+++ OnboardingControllerExamplesTests/OnboardingControllerExamplesTests.swift
@@ -22,8 +22,8 @@ class OnboardingControllerExamplesTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertTrue(false)
     }
     
 }
