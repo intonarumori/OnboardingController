8702c84343bbb299f1bf699e599251337deb2292
rumori <intonarumori@gmail.com>
2016-04-18 12:37:06 +0200

    updated tests

--- OnboardingControllerExamplesTests/OnboardingControllerExamplesTests.swift
+++ OnboardingControllerExamplesTests/OnboardingControllerExamplesTests.swift
@@ -13,17 +13,10 @@ class OnboardingControllerExamplesTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        
-        XCTAssertTrue(false)
     }
     
 }
